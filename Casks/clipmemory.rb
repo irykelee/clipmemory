08f1d345d508220c9d8e0e5ca9e1472a63c68412
irykelee <282283327+irykelee@users.noreply.github.com>
2026-05-15 14:20:37 +0800

    chore: update cask SHA256 for v2.1.0

--- Casks/clipmemory.rb
+++ Casks/clipmemory.rb
@@ -1,6 +1,6 @@
 	cask "clipmemory" do
 	  version "2.1.0"
-	  sha256 "eaa27fcdc38bbe889c140b65c25a6fb66482cc7d96d5fc43eeb75659a6f2f099"
+	  sha256 "e36a14ce4765f11b4c7ecd30a02eca62a909fb5c5753e20840ffdf9a2ac44fe4"
 
 	  url "https://github.com/irykelee/clipmemory/releases/download/v#{version}/ClipMemory.tar.gz"
 	  name "ClipMemory"
